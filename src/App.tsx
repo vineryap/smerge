d35a80fc73665f3813d97f857e9cad5c2c774bdb
Vin E. R. Yap <vineryap@gmail.com>
2022-01-24 22:12:49 +0900

    Refactor and added function to make SRT file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { Component, createSignal } from "solid-js";
-import { parseText, isIntervalslOverlap, sortSubtitleSection } from "./helpers";
+import { Component, createMemo, createSignal, Show } from "solid-js";
+
+import {
+  parseText,
+  isIntervalslOverlap,
+  sortSubtitleSection,
+  joinSubtitleSections
+} from "./helpers";
+import Layout from "./layouts/BaseLayout";
 import { subtitleObject } from "./types";
 
 const App: Component = () => {
   const [fileOne, setFileOne] = createSignal<File | null>(null);
   const [fileTwo, setFileTwo] = createSignal<File | null>(null);
+  const [mergedSubtitle, setMergedSubtitle] = createSignal<string>("");
+  const [isDownloadReady, setIsDownloadReady] = createSignal<boolean>(false);
 
   function getFile(inputEl: HTMLInputElement): File | null {
     return (inputEl.files as FileList)[0];
@@ -31,15 +40,28 @@ const App: Component = () => {
     }
   }
 
+  function makeSrtFile(text: string) {
+    const data = new Blob([text], { type: "text/plain" });
+    if (mergedSubtitle() !== null) {
+      window.URL.revokeObjectURL(mergedSubtitle());
+    }
+    setMergedSubtitle(window.URL.createObjectURL(data));
+  }
+
+  createMemo(() => {
+    if (mergedSubtitle()) {
+      setIsDownloadReady(true);
+    }
+  });
+
   async function mergeSubtitles(): Promise<void> {
     if (fileOne() && fileTwo()) {
       const fileOneSections = parseText(await fileOne()?.text());
       const fileTwoSections = parseText(await fileTwo()?.text());
 
       if (fileOneSections && fileTwoSections) {
-        let merged: subtitleObject[] = [];
+        let mergedSections: subtitleObject[] = [];
         const appendedIndex: number[] = [];
-        // console.log(fileOneSections);
 
         for (let index = 0; index < fileOneSections.length; index++) {
           let isFileOneOverlap = false;
@@ -50,7 +72,7 @@ const App: Component = () => {
 
             if (isIntervalslOverlap(fileOneSection.timestamp, fileTwoSection.timestamp)) {
               const text = fileOneSection.text.replace("\n\n", "\n") + fileTwoSection.text;
-              merged.push({
+              mergedSections.push({
                 timestamp: fileOneSection.timestamp,
                 text
               });
@@ -61,48 +83,58 @@ const App: Component = () => {
           }
           if (!isFileOneOverlap) {
             isFileOneOverlap = false;
-            merged.push(fileOneSection);
+            mergedSections.push(fileOneSection);
           }
         }
 
-        merged = sortSubtitleSection(
-          merged.concat(fileTwoSections.filter((s, i) => !appendedIndex.includes(i)))
+        mergedSections = sortSubtitleSection(
+          mergedSections.concat(fileTwoSections.filter((s, i) => !appendedIndex.includes(i)))
         );
+        makeSrtFile(joinSubtitleSections(mergedSections));
       }
     }
   }
+
   return (
-    <>
-      <div class="relative w-full min-h-screen mx-auto my-30">
-        <div class="container px-30">
-          <div class="flex flex-col justify-between items-center">
-            <div class="flex-row flex-wrap ">
-              <label htmlFor="subtitle_one">Subtitle 1</label>
-              <input
-                type="file"
-                name="subtitleOne"
-                id="subtitle_one"
-                accept=".srt"
-                onChange={setFileHandler}
-              />
-              <label htmlFor="subtitle_two">Subtitle 2</label>
-              <input
-                type="file"
-                name="subtitleTwo"
-                id="subtitle_two"
-                accept=".srt"
-                onChange={setFileHandler}
-              />
-            </div>
-          </div>
-          <div class="flex justify-center mt-20">
-            <button class="px-5 py-2 bg-blue-500" onclick={mergeSubtitles}>
-              Submit
-            </button>
+    <Layout>
+      <div class="container px-30">
+        <div class="flex flex-col justify-between items-center">
+          <div class="flex-row flex-wrap ">
+            <label htmlFor="subtitle_one">Subtitle 1</label>
+            <input
+              type="file"
+              name="subtitleOne"
+              id="subtitle_one"
+              accept=".srt"
+              onChange={setFileHandler}
+            />
+            <label htmlFor="subtitle_two">Subtitle 2</label>
+            <input
+              type="file"
+              name="subtitleTwo"
+              id="subtitle_two"
+              accept=".srt"
+              onChange={setFileHandler}
+            />
           </div>
         </div>
+        <div class="flex items-center justify-center mt-20">
+          <button class="px-5 py-2 bg-blue-500" onClick={mergeSubtitles}>
+            Submit
+          </button>
+        </div>
+        <div
+          classList={{ "opacity-100": isDownloadReady(), "opacity-0": !isDownloadReady() }}
+          class="flex items-center justify-center mt-20 transition-all"
+        >
+          <Show when={isDownloadReady()}>
+            <a href={mergedSubtitle()} download={"merged.srt"} class="px-5 py-2 bg-blue-500">
+              Download
+            </a>
+          </Show>
+        </div>
       </div>
-    </>
+    </Layout>
   );
 };
 
