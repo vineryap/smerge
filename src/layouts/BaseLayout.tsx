f54362cd46d6f6875e61ae7724e7dc4b75ed2fd0
Vin E. R. Yap <vineryap@gmail.com>
2022-01-30 14:11:58 +0900

    Updated layout.

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,10 +1,10 @@
 import { Component } from "solid-js";
+import Header from "../components/Header";
 
 const Layout: Component = props => (
-  <div class="relative overflow-hidden h-screen">
-    <div class="flex flex-col flex-wrap h-full w-full px-5 justify-center items-center">
-      {props.children}
-    </div>
+  <div id="main" class="relative overflow-auto h-screen">
+    <Header />
+    <div class="flex flex-col flex-wrap w-full px-5 items-center">{props.children}</div>
   </div>
 );
 
