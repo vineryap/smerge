caf0864519143517fa34a7729fc0d47d90087931
Vin E. R. Yap <vineryap@gmail.com>
2022-01-31 13:50:04 +0900

    Refactor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,106 @@
-import { Component, createMemo, createSignal, Show } from "solid-js";
+import { Component, createMemo, createSignal, For, Show } from "solid-js";
 import FileInput from "./components/FileInput";
 import Layout from "./layouts/BaseLayout";
 import { merge, Subtitle } from "subtitle-merger";
 
 const App: Component = () => {
-  const [fileOne, setFileOne] = createSignal<File | null>(null);
-  const [fileTwo, setFileTwo] = createSignal<File | null>(null);
+  const [addedFiles, setFiles] = createSignal<File[]>([]);
   const [subtitle, setSubtitle] = createSignal<Subtitle | null>(null);
-  const [mergedSubtitle, setMergedSubtitle] = createSignal<string>("");
+  const [outputName, setOutputName] = createSignal<string>("");
   const [isDownloadReady, setIsDownloadReady] = createSignal<boolean>(false);
 
-  function getFile(inputEl: HTMLInputElement): File | null {
-    return (inputEl.files as FileList)[0];
-  }
+  const output = createMemo(() => {
+    if (!subtitle()) return;
 
-  function fileInputHandler(e: Event) {
-    const target = e.target as HTMLInputElement;
-    const id = target.id;
-    const file = getFile(target);
-
-    if (!file?.name.match(/\.srt$/i)) {
-      console.error("only .srt file is supported.");
-      return;
-    }
-
-    switch (id) {
-      case "subtitle_one":
-        setFileOne(file);
-        break;
-
-      default:
-        setFileTwo(file);
-        break;
-    }
-  }
-
-  createMemo(() => {
-    if (subtitle() && !isDownloadReady()) {
-      const data = subtitle()?.blob as Blob | MediaSource;
-
-      if (mergedSubtitle() !== null) {
-        window.URL.revokeObjectURL(mergedSubtitle());
-      }
-      setMergedSubtitle(window.URL.createObjectURL(data));
-
-      setIsDownloadReady(true);
-    }
+    const data = subtitle()?.blob as Blob | MediaSource;
+    setIsDownloadReady(true);
+    return window.URL.createObjectURL(data);
   });
 
+  function resetStates() {
+    setIsDownloadReady(false);
+    setSubtitle(null);
+  }
+
+  function filesHandler(files: File[]) {
+    if (!files.length) return;
+    if (files.map(f => f.name).filter(n => !n.match(/\.srt$/i)).length) return;
+    resetStates();
+    if (addedFiles().length === 1) setFiles(current => current.concat(files[0]));
+    else setFiles(files.slice(0, 2));
+  }
+
+  const mergeDisabled = createMemo(() => addedFiles().length < 2 || !!output());
+
   async function mergeSubtitles() {
-    if (fileOne() && fileTwo()) {
-      const merged = merge((await fileOne()?.text()) || "", (await fileTwo()?.text()) || "");
-      setSubtitle(merged);
-    }
+    if (mergeDisabled()) return;
+
+    const [fileOne, fileTwo] = addedFiles();
+    const fileOneText = await fileOne.text();
+    const fileTwoText = await fileTwo.text();
+    const merged = merge(fileOneText, fileTwoText);
+    setSubtitle(merged);
   }
 
   return (
     <Layout>
       <div class="my-15 flex flex-col items-center sm:max-w-2xl w-full p-5 sm:p-10 bg-white rounded-xl shadow-xl transition-all">
         <div class="text-center mb-10">
-          <h2 class="mt-5 text-3xl font-bold text-gray-900">Subtitle Merger</h2>
+          <h1 class="mt-5 text-3xl font-bold text-gray-900">Subtitle Merger</h1>
           <p class="mt-2 text-sm text-gray-400">
             Merge two subtitles into one. Files are not sent to the server which means no data are
             collected.
           </p>
         </div>
-        <div class="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
+        <div class="flex flex-col sm:flex-row w-full h-full space-y-4 sm:space-y-0 sm:space-x-4">
           <FileInput
-            label="Add Subitle One"
+            label="Add two subtitles to be merged"
             inputId="subtitle_one"
-            fileInputHandler={fileInputHandler}
+            filesHandler={filesHandler}
           />
-          <FileInput
-            label="Add Subitle Two"
-            inputId="subtitle_two"
-            fileInputHandler={fileInputHandler}
-          />
+        </div>
+        <ul class="block w-full justify-start mt-10 list-inside list-disc">
+          <h3 class="font-bold text-gray-500 tracking-wide">Added Files</h3>
+          <For
+            each={addedFiles()}
+            fallback={<span class="font-semibold text-gray-400 text-sm">None</span>}
+          >
+            {file => <li class="font-semibold text-sm">{file.name}</li>}
+          </For>
+        </ul>
+        <div class="w-full flex flex-col flex-wrap mt-10 space-y-2">
+          <label class="font-bold text-gray-500 tracking-wide">Output Filename</label>
+          <input
+            class="text-base p-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring focus:ring-blue-500 focus:border-gray-100 transition"
+            type="text"
+            placeholder="merged.srt"
+            value={outputName()}
+            onChange={e => setOutputName((e.target as HTMLInputElement).value)}
+          ></input>
         </div>
         <div class="w-full max-w-70">
           <button
             type="submit"
-            class="mt-10 w-full flex justify-center bg-blue-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-blue-700 shadow-lg cursor-pointer transition ease-in"
+            class="mt-10 w-full flex justify-center text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline shadow-lg transition ease-in"
+            className={`${
+              mergeDisabled()
+                ? "bg-gray-400 hover:bg-gray-400 cursor-auto"
+                : "bg-blue-600 hover:bg-blue-700 cursor-pointer"
+            }`}
             onClick={mergeSubtitles}
+            disabled={mergeDisabled()}
           >
             Merge
           </button>
         </div>
         <div
-          classList={{ "opacity-100": isDownloadReady(), "opacity-0": !isDownloadReady() }}
+          className={`${isDownloadReady() ? "opacity-100" : "opacity-0"}`}
           class="w-full max-w-70"
         >
           <Show when={isDownloadReady()}>
             <a
-              href={mergedSubtitle()}
-              download={"merged.srt"}
+              href={output()}
+              download={outputName() ? outputName() + ".srt" : "merged.srt"}
               class="mt-5 w-full flex justify-center bg-purple-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold focus:outline-none focus:shadow-outline hover:bg-purple-700 shadow-lg cursor-pointer transition ease-in"
             >
               Save
